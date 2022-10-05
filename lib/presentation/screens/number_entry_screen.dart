925a2916cd239b9c2dcdff72d14fe7ce9e9d38dd
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-05 11:39:20 +0300

    created NumberEntryScreen and NumberEntryLayout

---
+++ lib/presentation/screens/number_entry_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get_started_app/presentation/screens/number_entry_layout.dart';
+
+class NumberEntryScreen extends StatelessWidget {
+  const NumberEntryScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const NumberEntryLayout();
+  }
+}
