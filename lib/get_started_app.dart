f56ff537d85975b01bd5770937b6954beca5e831
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-05 11:40:31 +0300

    wrap in SafeArea NumberEntryScreen

--- lib/get_started_app.dart
+++ lib/get_started_app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'presentation/screens/number_entry_screen.dart';
+
 class GetStartedApp extends StatelessWidget {
   const GetStartedApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(
+      home: SafeArea(child: NumberEntryScreen()),
+    );
   }
 }
