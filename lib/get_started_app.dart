fc416401b8d6ce81b87d59c030d2961f891df52d
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-05 10:54:42 +0300

    Initial commit

---
+++ lib/get_started_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GetStartedApp extends StatelessWidget {
+  const GetStartedApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:get_started_app/get_started_app.dart';
+
+void main() {
+  runApp(const GetStartedApp());
+}
