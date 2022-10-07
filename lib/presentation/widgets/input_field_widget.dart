f740b2ad38ac35edbc6f40fd9210ef932f4bee11
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-07 15:48:49 +0300

    refactored InputForm

--- lib/presentation/widgets/input_field_widget.dart
+++ lib/presentation/widgets/input_field_widget.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 typedef ValidatorFunction = String? Function(String?);
 
 class InputFieldWidget extends StatelessWidget {
   const InputFieldWidget({
-    Key? key,
+    super.key,
     required this.labelText,
     this.validator,
-  }) : super(key: key);
+    this.node,
+    this.inputFormatters,
+    this.onChanged,
+    this.onSaved,
+    this.onFieldSubmitted,
+  });
 
   final String labelText;
   final ValidatorFunction? validator;
+  final FocusNode? node;
+  final List<TextInputFormatter>? inputFormatters;
+  final void Function(String)? onChanged;
+  final void Function(String)? onFieldSubmitted;
+  final void Function(String?)? onSaved;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onFieldSubmitted: onFieldSubmitted,
+      onSaved: onSaved,
+      onChanged: onChanged,
+      inputFormatters: inputFormatters,
+      focusNode: node,
       keyboardType: TextInputType.number,
       decoration: InputDecoration(
         enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(16.0)),
--- lib/presentation/widgets/input_form_widget.dart
+++ lib/presentation/widgets/input_form_widget.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_started_app/presentation/models/country_model.dart';
 import 'package:get_started_app/presentation/screens/form_validators.dart';
 import 'package:get_started_app/presentation/widgets/input_field_widget.dart';
 
 class InputFormWidget extends StatefulWidget {
-  const InputFormWidget({Key? key}) : super(key: key);
+  const InputFormWidget({Key? key, required this.onTap, required this.onValidationChanged}) : super(key: key);
+
+  final Future<CountryModel?> Function() onTap;
+  final void Function(bool) onValidationChanged;
 
   @override
   State<InputFormWidget> createState() => _MyLogFormWidgetState();
@@ -12,17 +17,89 @@ class InputFormWidget extends StatefulWidget {
 
 class _MyLogFormWidgetState extends State<InputFormWidget> {
   final formKey = GlobalKey<FormState>();
+  final textController = TextEditingController();
+  final fullPhoneNode = FocusNode();
+
+  bool isFullPhoneFieldValid = false;
 
   @override
   Widget build(BuildContext context) {
     return Form(
         key: formKey,
-        child: Expanded(
-          flex: 3,
-          child: InputFieldWidget(
-            labelText: '(123) 123-1234',
-            validator: FormValidators.numberValidator,
-          ),
+        child: Row(
+          children: [
+            Flexible(
+              flex: 3,
+              child: InkWell(
+                onTap: () async {
+                  final country = await widget.onTap();
+                  String code = '';
+                  if (country == null) {
+                    FocusManager().primaryFocus?.unfocus();
+                  } else {
+                    code = country.flagAndCode;
+                    fullPhoneNode.requestFocus();
+                  }
+                  textController.text = code;
+                },
+                child: TextFormField(
+                  enabled: false,
+                  controller: textController,
+                ),
+              ),
+            ),
+            const SizedBox(width: 20),
+            Flexible(
+              flex: 7,
+              child: InputFieldWidget(
+                labelText: '(123) 123-1234',
+                onFieldSubmitted: (_) => formKey.currentState?.validate(),
+                validator: (input) {
+                  final error = FormValidators.numberValidator(input);
+                  final isValid = error == null;
+                  if (isFullPhoneFieldValid != isValid) {
+                    isFullPhoneFieldValid = isValid;
+                    widget.onValidationChanged(isFullPhoneFieldValid);
+                  }
+
+                  return error;
+                },
+                node: fullPhoneNode,
+                inputFormatters: <TextInputFormatter>[
+                  InternationalPhoneFormatter(),
+                ],
+              ),
+            ),
+          ],
         ));
+  }
+}
+
+// todo: move to another file
+class InternationalPhoneFormatter extends TextInputFormatter {
+  String internationalPhoneFormat(value) {
+    String nums = value.replaceAll(RegExp(r'[\D]'), '');
+    String internationalPhoneFormatted = nums.substring(0, nums.isNotEmpty ? 0 : null) +
+        (nums.isNotEmpty ? ' (' : '') +
+        nums.substring(0, nums.length >= 3 ? 3 : null) +
+        (nums.length > 3 ? ') ' : '') +
+        (nums.length > 3
+            ? nums.substring(3, nums.length >= 6 ? 6 : null) +
+                (nums.length > 6 ? ' ${nums.substring(6, nums.length >= 10 ? 10 : null)}' : '')
+            : '');
+    return internationalPhoneFormatted;
+  }
+
+  @override
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+    String text = newValue.text;
+
+    if (newValue.selection.baseOffset == 0) {
+      return newValue;
+    }
+
+    return newValue.copyWith(
+        text: internationalPhoneFormat(text),
+        selection: TextSelection.collapsed(offset: internationalPhoneFormat(text).length));
   }
 }
