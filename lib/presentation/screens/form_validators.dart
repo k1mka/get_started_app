58bc00c69a1c37e51fbd4538b4899a5d42c54d8a
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-07 15:47:39 +0300

    changed validation

--- lib/presentation/screens/form_validators.dart
+++ lib/presentation/screens/form_validators.dart
@@ -1,12 +1,9 @@
 import 'package:get_started_app/presentation/widgets/input_field_widget.dart';
 
 class FormValidators {
-  static final _emailRegexp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}');
-  // TODO: CHANGE VALIDATION
-
   static ValidatorFunction numberValidator = (value) {
     if (value == null || value.isEmpty) return 'Please, enter phone number';
-    if (!_emailRegexp.hasMatch(value)) return 'Please, enter correct phone number';
+    if (value.length < 15) return 'Please, enter correct phone number';
     return null;
   };
 }
