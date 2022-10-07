06c000b297bf9683e5e8f0c589dc762d62c5e734
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-07 15:48:07 +0300

    made custom button

--- lib/presentation/widgets/custom_button.dart
+++ lib/presentation/widgets/custom_button.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  CustomButton({Key? key, required this.onPressed}) : super(key: key);
+  CustomButton({Key? key, required this.onPressed, required this.enabled}) : super(key: key);
 
   final VoidCallback onPressed;
-  double minimalSize = 40;
+  static const double minimalSize = 20;
+
+  final bool enabled;
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-        onPressed: onPressed,
-        icon: Icon(
-          Icons.next_plan_outlined,
-          size: minimalSize,
-        ));
+    return FloatingActionButton(
+      elevation: 0,
+      backgroundColor: enabled ? Colors.white : const Color.fromRGBO(244, 245, 255, 0.4),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      onPressed: enabled ? onPressed : null,
+      child: Icon(
+        color: enabled ? Colors.black : Colors.white,
+        Icons.arrow_forward,
+        size: minimalSize,
+      ),
+    );
   }
 }
