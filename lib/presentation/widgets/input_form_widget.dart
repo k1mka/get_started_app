3e75ac9fa2fac7cac1a95b6d16010a9d59386a18
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-07 13:02:41 +0300

    refactored inputField

--- lib/presentation/widgets/input_form_widget.dart
+++ lib/presentation/widgets/input_form_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get_started_app/presentation/screens/form_validators.dart';
 import 'package:get_started_app/presentation/widgets/input_field_widget.dart';
 
@@ -15,26 +16,13 @@ class _MyLogFormWidgetState extends State<InputFormWidget> {
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: formKey,
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: InputFieldWidget(
-              labelText: '(conutry)',
-              validator: FormValidators.numberValidator,
-            ),
+        key: formKey,
+        child: Expanded(
+          flex: 3,
+          child: InputFieldWidget(
+            labelText: '(123) 123-1234',
+            validator: FormValidators.numberValidator,
           ),
-          const SizedBox(width: 20),
-          Expanded(
-            flex: 3,
-            child: InputFieldWidget(
-              labelText: '(123) 123-1234',
-              validator: FormValidators.numberValidator,
-            ),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
