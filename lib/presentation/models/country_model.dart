c8b5e58483681852e2962fbe85752875e94e3468
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-07 13:01:34 +0300

    added new field, and unite method to model

--- lib/presentation/models/country_model.dart
+++ lib/presentation/models/country_model.dart
@@ -1,17 +1,20 @@
 class CountryModel {
   final String flag;
   final String region;
-  final String subRegion;
-  final String phoneID;
+  final String phoneID; // TODO: RENAMED
+  final String phoneSuffix;
+
   CountryModel({
     required this.phoneID,
+    required this.phoneSuffix,
     required this.flag,
     required this.region,
-    required this.subRegion,
   });
+
+  String get phoneCode => '$phoneID$phoneSuffix';
 
   @override
   String toString() {
-    return '{flag: $flag, region: $region, subRegion: $subRegion, phoneID: $phoneID}';
+    return 'CountryModel{flag: $flag, region: $region, phoneID: $phoneID, phoneSuffix: $phoneSuffix}';
   }
 }
